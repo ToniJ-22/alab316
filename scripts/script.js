93865f170145f1318d180cef08839d4dec7e2b51
ToniJ-22 <rachelrene1524@gmail.com>
2025-12-16 18:06:36 -0500

    https://github.com/ToniJ-22/alab316.git

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 //PART 1 constructing main 
 let mainEl= document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
-mainEl.innerHTML = <h1>DOM Manipulation</h1>;
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr');
 //PART 2 building nav bar
 let topMenuEl= document.getElementById('top-menu');
-topMenuEl.style.height('100%');
-mainEl.style.backgroundColor= 'var(--top-menu-bg)';
-mainEl.classList.add('flex-around');
+topMenuEl.style.height='100%';
+topMenuEl.style.backgroundColor= 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
 //PART 3
 var menuLinks = [
   { text: 'about', href: '/about' },
@@ -19,5 +19,5 @@ for (let link of menuLinks){
     let anchor = document.createElement('a');
     anchor.setAttribute('href', link.href);
     anchor.textContent = link.text;
-    logMenuEl.apprendChild(anchor);
+    topMenuEl.appendChild(anchor);
 }
